e6c4080670b8258d00d8e01a980fd9b1aecb8b2d
Guilherme Martins <guihenriquemvieira@gmail.com>
2026-09-23 16:15:22 -0300

    M00: primeiro programa do projeto Arquivo Vivo

---
+++ OlaArquivoVivo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OlaArquivoVivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("=== Projeto Arquivo Vivo ===");
+            Console.WriteLine("Contratante: Prefeitura de Vale Verde");
+            Console.WriteLine("Contrato nº 087/2026");
+            Console.WriteLine();
+
+            Console.Write("Qual é o seu nome? ");
+            string? nome = Console.ReadLine();
+
+            Console.WriteLine($"Bem-vindo à DocsPrime, {nome}!");
+            Console.WriteLine($"Hoje é {DateTime.Now:dd/MM/yyyy}. Bom trabalho!");
+        }
+    }
+}
