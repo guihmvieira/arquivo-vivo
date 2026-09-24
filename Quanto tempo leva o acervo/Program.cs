72b3a77c7c5295d40a6ec88e7d944176c51a9b7a
Guilherme Martins <guihenriquemvieira@gmail.com>
2026-09-24 16:06:50 -0300

    M01: aula 1: exercícios de variáveis e tipos

---
+++ Quanto tempo leva o acervo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace M01.Logica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            CultureInfo.CurrentCulture = new CultureInfo("pt-BR");
+
+            // Dados do mês
+            int totalPaginas = 400000;
+            int paginasPorMes = 42000;
+
+            // Cálculos
+            int mesesCompletos = totalPaginas / paginasPorMes;
+            int paginasQueSobram = totalPaginas % paginasPorMes;
+
+            // Relatório
+            Console.WriteLine($"Total de Páginas: {totalPaginas:N0}");
+            Console.WriteLine($"Produção Mensal: {paginasPorMes:N0}");
+            Console.WriteLine($"Meses Completos: {mesesCompletos}");
+            Console.WriteLine($"Páginas que sobram para o mês seguinte: {paginasQueSobram}");
+
+            long cpfComoNumero = 00001234567890;
+            Console.WriteLine(cpfComoNumero);
+        }
+    }
+}
